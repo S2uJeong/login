a72a6ee6f129d76f825ae0776ea17f5f5452fabb
sujeong <sj980204@gmail.com>
2024-04-07 16:15:13 +0900

    [🚀feat] 회원가입 기능

---
+++ src/main/java/hello/login/domain/member/MemberController.java
@@ -0,0 +1,33 @@
+package hello.login.domain.member;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/add")
+    public String addForm(@ModelAttribute("member") Member member) {
+        return "members/addMemberForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@Valid @ModelAttribute Member member, BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/addMemberForm";
+        }
+        memberRepository.save(member);
+        return "redirect:/";
+    }
+
+}
